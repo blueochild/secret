6e0a9764284b363c6415a47034510179df765aa6
blueochild <blueochild@gmail.com>
2020-09-23 14:18:33 +0900

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,68 @@
+window.onload = function () {
+    var elm = ".box";
+    $(elm).each(function (index) {
+        // 개별적으로 Wheel 이벤트 적용
+        $(this).on("mousewheel DOMMouseScroll", function (e) {
+            e.preventDefault();
+            var delta = 0;
+            if (!event) event = window.event;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                if (window.opera) delta = -delta;
+            } 
+            else if (event.detail)
+                delta = -event.detail / 3;
+            var moveTop = $(window).scrollTop();
+            var elmSelecter = $(elm).eq(index);
+            // 마우스휠을 위에서 아래로
+            if (delta < 0) {
+                if ($(elmSelecter).next() != undefined) {
+                    try{
+                        moveTop = $(elmSelecter).next().offset().top;
+                    }catch(e){}
+                }
+            // 마우스휠을 아래에서 위로
+            } else {
+                if ($(elmSelecter).prev() != undefined) {
+                    try{
+                        moveTop = $(elmSelecter).prev().offset().top;
+                    }catch(e){}
+                }
+            }
+             
+            // 화면 이동 0.8초(800)
+            $("html,body").stop().animate({
+                scrollTop: moveTop + 'px'
+            }, {
+                duration: 800, complete: function () {
+                }
+            });
+        });
+    });
+
+}
+
+function timer_btn(){
+    var timer_display = document.getElementById('timer_display');
+    if(timer_display.style.display == 'none'){
+        timer_display.style.display = 'block';
+    }
+    else if(timer_display.style.display == 'block'){
+        timer_display.style.display = 'none';
+    }
+}
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadeinleft').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+            
+        }); 
+    });
+});
